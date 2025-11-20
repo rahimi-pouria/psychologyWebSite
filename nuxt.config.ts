2d440aef1831d5c9374cfdeda9ef713d06d2bc42
pouriaRahimy <pouria.rahimy@gmail.com>
2025-11-20 08:39:55 +0330

    add bootstrap 5 in nuxt 4

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+    css: [
+        'bootstrap/dist/css/bootstrap.min.css',
+        '~/assets/css/main.css'
+    ],
+    plugins: ['./plugins/bootstrap.client.ts'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})
