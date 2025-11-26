dbfb7da9b29171165869a6394163b651092a9186
pouriaRahimy <pouria.rahimy@gmail.com>
2025-11-26 17:04:08 +0330

    create home page

---
+++ components/home/slider.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="@/public/image/png/3fd237a0-b1a6-41e2-97d3-a78896ef170c-1764159381.png" class="d-block w-100" alt="...">
+        <div class="overlay"></div>
+      </div>
+      <div class="carousel-item">
+        <img src="@/public/image/png/823bd78c-22db-4fe8-99dd-e1c114479ed2.png" class="d-block w-100" alt="...">
+        <div class="overlay"></div>
+      </div>
+      <div class="carousel-item">
+        <img src="@/public/image/png/b7229745-5f2d-4542-8df6-e71bc25f3825-1764159279.png" class="d-block w-100" alt="...">
+        <div class="overlay"></div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.carousel-inner {
+  height: 130vh;
+}
+
+.carousel-item {
+  position: relative;
+}
+
+.carousel-item img {
+  height: 100%;
+  object-fit: cover;
+  object-position: center top;
+  margin-top: -15rem;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 1;
+}
+
+span.carousel-control-prev-icon,
+span.carousel-control-next-icon {
+  border: 1px solid #fff;
+  border-radius: 50%;
+  padding: 25px;
+  background-size: 50%;
+  z-index: 2;
+  position: relative;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,51 @@
 <template>
-  <div class="d-flex flex-column gap-4">
-    <div class="d-flex flex-column">
-      <toolbar />
-      <div class="d-flex">
-        <header class="header-mind-nest d-flex flex-column mx-auto">
+  <div class="layout">
+    <div class="header-slider-wrapper">
+      <Slider />
+      <header class="header-mind-nest">
+        <div class="flex sub-header">
+          <toolbar />
+        </div>
+        <div class="flex header-navbar">
           <navbar />
-        </header>
-      </div>
+        </div>
+      </header>
     </div>
+
     <main class="container-fluid">
       <slot />
     </main>
+
     <footer>footer</footer>
   </div>
-
 </template>
 
 <script setup lang="ts">
-  import toolbar from '~/components/header/toolbar.vue'
-  import navbar from '~/components/header/navbar.vue'
+import toolbar from '~/components/header/toolbar.vue'
+import navbar from '~/components/header/navbar.vue'
+import Slider from '@/components/home/slider.vue'
 </script>
 
 <style scoped>
+.header-slider-wrapper {
+  position: relative;
+}
 
+.header-mind-nest {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 10;
+}
+.header-navbar{
+  background-color: white;
+  width: 70%;
+  display: flex;
+  margin: 0 auto;
+}
+
+header + Slider {
+  z-index: 1;
+}
 </style>
